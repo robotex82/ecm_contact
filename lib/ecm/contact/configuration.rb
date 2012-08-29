9918ef7449cec75227f76315e574e8602940d8b7
Roberto Vasquez Angel <robotex@robotex.de>
2012-08-30 00:34:27 +0200

    recipients is a has with indifferent access now.

--- lib/ecm/contact/configuration.rb
+++ lib/ecm/contact/configuration.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/module/delegation'
 require 'active_support/core_ext/module/attribute_accessors'
+require 'active_support/hash_with_indifferent_access'
 
 module Ecm
   module Contact
@@ -10,6 +11,10 @@ module Ecm
 
       mattr_accessor :recipients
       @@recipients = {}
+      
+      def recipients=(recipients)
+        @@recipients = HashWithIndifferentAccess.new(recipients)
+      end
     end
   end
 end
--- lib/ecm/contact/version.rb
+++ lib/ecm/contact/version.rb
@@ -1,5 +1,5 @@
 module Ecm
   module Contact
-    VERSION = '1.0.0.pre'
+    VERSION = '1.0.1.pre'
   end
 end    
