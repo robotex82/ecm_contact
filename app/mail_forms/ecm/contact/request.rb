a7b9431cbb86aa5943485c3f178471d579f6136a
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-24 09:59:28 +0200

    Fixed i18n issues.

--- app/mail_forms/ecm/contact/request.rb
+++ app/mail_forms/ecm/contact/request.rb
@@ -8,16 +8,16 @@ module Ecm
       attribute :terms_of_service
       attribute :nickname,  :captcha  => true    
       
-      validates :email, :format => { :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, :message => I18n.t("activerecord.errors.messages.invalid") }
-      validates :message, :presence => { :message => I18n.t("activerecord.errors.messages.empty") }
-      validates :name, :presence => { :message => I18n.t("activerecord.errors.messages.empty") }
-      validates :terms_of_service, :acceptance => { :message => I18n.t("activerecord.errors.messages.accepted") }
+      validates :email, :format => { :with => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i }, :presence => true # , :message => Proc.new { I18n.t("activerecord.errors.messages.invalid") }.call }
+      validates :message, :presence => true # { :message => Proc.new { I18n.t("activerecord.errors.messages.empty") }.call }
+      validates :name, :presence => true # { :message => Proc.new { I18n.t("activerecord.errors.messages.empty") }.call }
+      validates :terms_of_service, :acceptance => true # { :message => Proc.new { I18n.t("activerecord.errors.messages.accepted") }.call }
 
       # Declare the e-mail headers. It accepts anything the mail method
       # in ActionMailer accepts.
       def headers
         {
-          :subject => I18n.t('ecm.contact.request.subject', :application_name => Rails.application.class.to_s.split("::").first),
+          :subject => Proc.new { I18n.t('ecm.contact.request.subject', :application_name => Rails.application.class.to_s.split("::").first) }.call,
           # :to => I18n.t('ecm.contact.request.recipients'),
           :to => Ecm::Contact::Configuration.recipients[Rails.env],
           :from => %("#{name}" <#{email}>)
--- lib/ecm/contact/version.rb
+++ lib/ecm/contact/version.rb
@@ -1,5 +1,5 @@
 module Ecm
   module Contact
-    VERSION = '1.0.2.pre'
+    VERSION = '1.0.3.pre'
   end
 end    
