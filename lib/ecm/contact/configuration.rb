4f06a66481761b2ec78224b3334bcd8b9d9b0cc0
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-24 10:34:34 +0200

    Configurable css classes for inputs.

--- lib/ecm/contact/configuration.rb
+++ lib/ecm/contact/configuration.rb
@@ -8,6 +8,20 @@ module Ecm
       def configure
         yield self
       end
+      mattr_accessor :input_name_css_classes
+      @@input_name_css_classes = nil
+
+      mattr_accessor :input_email_css_classes
+      @@input_email_css_classes = nil
+
+      mattr_accessor :input_phone_css_classes
+      @@input_phone_css_classes = nil
+
+      mattr_accessor :input_message_css_classes
+      @@input_message_css_classes = nil
+
+      mattr_accessor :input_terms_of_service_css_classes
+      @@input_terms_of_service_css_classes = nil
 
       mattr_accessor :recipients
       @@recipients = {}
--- lib/ecm/contact/version.rb
+++ lib/ecm/contact/version.rb
@@ -1,5 +1,5 @@
 module Ecm
   module Contact
-    VERSION = '1.0.3.pre'
+    VERSION = '1.0.4.pre'
   end
 end    
